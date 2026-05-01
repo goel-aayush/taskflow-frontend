8cb9a1b42b3332b65986b426cba7dbcd637685f1
aayush <mgoel1296@gmail.com>
2026-05-01 18:18:10 +0530

    fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://taskflow-production-4318.up.railway.app/",
+  baseURL: "https://taskflow-production-4318.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
